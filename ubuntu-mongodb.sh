ae28a99e89865bd32e3fd0691e10ec017304a020
mrdcvlsc <mrdcvlsc@gmail.com>
2024-12-25 22:51:38 +0800

    added github workflow

--- ubuntu-mongodb.sh
+++ ubuntu-mongodb.sh
@@ -6,11 +6,16 @@
 set -e
 
 # MongoDB version and source information
-MONGO_VERSION="r8.0.4"
 MONGO_VERSION_RAW="8.0.4"
-MONGO_SOURCE_URL="https://github.com/mongodb/mongo/archive/refs/tags/$MONGO_VERSION.tar.gz"
+# MONGO_VERSION_RAW="7.0.16"
 
-# Dependencies installation
+RELEASE_MONGO_VERSION="r$MONGO_VERSION_RAW"
+MONGO_SOURCE_URL="https://github.com/mongodb/mongo/archive/refs/tags/$RELEASE_MONGO_VERSION.tar.gz"
+
+echo "============================================================"
+echo "Dependencies installation"
+echo "============================================================"
+
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y \
@@ -22,34 +27,86 @@ sudo apt-get install -y \
     python3-pip \
     ninja-build \
     git \
-    libssl-dev
+    libssl-dev \
+    python-dev-is-python3
 
-# Download and extract MongoDB source
-curl -L $MONGO_SOURCE_URL -o mongo-$MONGO_VERSION.tar.gz
-tar -xzf mongo-$MONGO_VERSION.tar.gz
-cd mongo-$MONGO_VERSION
+echo "============================================================"
+echo "Download and extract MongoDB source"
+echo "============================================================"
 
-# Set up Python environment
-python3 -m venv python3-venv --prompt mongo
-source python3-venv/bin/activate
+curl -L $MONGO_SOURCE_URL -o mongo-$RELEASE_MONGO_VERSION.tar.gz
+tar -xzf mongo-$RELEASE_MONGO_VERSION.tar.gz
+cd mongo-$RELEASE_MONGO_VERSION
 
-# Install Python dependencies
-pip install --upgrade pip
-python3 -m pip install 'poetry==1.8.3'
-python3 -m poetry install --no-root --sync
+echo "============================================================"
+echo "Set up environment variables for clang and architecture compatibility"
+echo "============================================================"
 
-# Set up environment variables for clang and architecture compatibility
 export CC=clang
 export CXX=clang++
 export CFLAGS="-march=x86-64 -mtune=generic -O3"
 export CCFLAGS="-march=x86-64 -mtune=generic -O3"
 export CXXFLAGS="-march=x86-64 -mtune=generic -O3"
 
-# Build MongoDB
+echo "============================================================"
+echo "Set up Python environment"
+echo "============================================================"
+
+python3 -m venv python3-venv --prompt mongo
+source python3-venv/bin/activate
+
+echo "============================================================"
+echo "Install Python dependencies"
+echo "============================================================"
+pip install --upgrade pip
+echo "------------------------------------------------------------"
+python3 -m pip install setuptools
+echo "------------------------------------------------------------"
+python3 -m pip install wheel
+echo "------------------------------------------------------------"
+python3 -m pip install distlib
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'pymongo==4.3.3'
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'zope-interface==5.0.0'
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'sentinels==1.0.0'
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'psutil==5.8.0'
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'pyyaml==5.3.1'
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'cheetah3==3.2.6.post1'
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'requests-oauth==0.4.1'
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'pykmip==0.10.0'
+echo "------------------------------------------------------------"
+python3 -m pip install --no-use-pep517 'regex==2021.11.10'
+echo "------------------------------------------------------------"
+
+python3 -m pip install 'poetry==1.8.3'
+
+echo "------------------------------------------------------------"
+
+python3 -m poetry install --no-root --sync
+
+if uname -a | grep -q 's390x\|ppc64le'; then
+    echo "-------------------cryptography-----------------------------"
+    python3 -m pip uninstall -y cryptography==2.3
+    python3 -m pip install cryptography==2.3
+fi
+
+echo "============================================================"
+echo "Build MongoDB"
+echo "============================================================"
+
 python3 buildscripts/scons.py MONGO_VERSION=$MONGO_VERSION_RAW install-mongod --jobs=2 --disable-warnings-as-errors --linker=gold
 
 # Inform the user of the build location
+echo "============================================================"
 echo "MongoDB build complete. Binaries are located in the 'build/install/bin' directory."
+echo "============================================================"
 
 # Cleanup (optional)
 deactivate
