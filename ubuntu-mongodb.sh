bccb261973212c5336aab2dc88731d5001abea87
Jubal Mordecai Velasco <mrdcvlsc@gmail.com>
2024-12-25 12:28:56 +0000

    updated script

--- ubuntu-mongodb.sh
+++ ubuntu-mongodb.sh
@@ -12,6 +12,7 @@ MONGO_SOURCE_URL="https://github.com/mongodb/mongo/archive/refs/tags/$MONGO_VERS
 
 # Dependencies installation
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y \
     build-essential \
     libcurl4-openssl-dev \
